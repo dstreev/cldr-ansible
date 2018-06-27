df6bcd69e82af327bb594f764e867e277c6b9a61
David Streever <david@streever.com>
2018-06-27 21:49:56 +0200

    Fixing Partition.

--- hdp/sdlc/files/acid_insert_2.sql
+++ hdp/sdlc/files/acid_insert_2.sql
@@ -2,7 +2,7 @@ USE acid_${HIVE_USER};
 
 
 FROM raw_2_dataset
-INSERT INTO acid_2_dataset PARTITION (part='${PART}')
+INSERT INTO acid_2_dataset PARTITION (part)
 SELECT event_load_time,
        filename,
        id,
@@ -24,9 +24,9 @@ SELECT event_load_time,
        terminate_duration,
        terminate_state,
        i01,
-       i02
-WHERE part='${PART}'
-  INSERT INTO append_2_dataset
+       i02,
+       '${PART}'
+INSERT INTO append_2_dataset PARTITION (part)
 SELECT event_load_time,
        filename,
        id,
@@ -48,39 +48,37 @@ SELECT event_load_time,
        terminate_duration,
        terminate_state,
        i01,
-       i02
-WHERE part='${PART}';
+       i02,
+       '${PART}';
 
 -- Build Merge Statement
 
-MERGE INTO merge_2_dataset PARTITION (part='${PART}') AS T USING
-SELECT *
-FROM raw_2_dataset
-WHERE part='${PART}' AS S ON
-  T.event_load_time = S.event_load_time
-  AND T.filename = S.filename
-  AND T.id = S.id
-  WHEN NOT MATCHED THEN
-  INSERT
-  VALUES (S.event_load_time,
-          S.filename,
-          S.id,
-          S.status,
-          S.status_comment,
-          S.event_real_start,
-          S.event_real_stop,
-          S.event_planned_start,
-          S.event_planned_stop,
-          S.total_event_duration,
-          S.event_comment,
-          S.cell_id,
-          S.cell_site,
-          S.cell_node,
-          S.equipment_name,
-          S.cell_terminate,
-          S.terminate_start,
-          S.terminate_stop,
-          S.terminate_duration,
-          S.terminate_state,
-          S.i01,
-          S.i02);
+MERGE INTO merge_2_dataset PARTITION (part) AS T
+USING raw_2_dataset AS S ON
+T.event_load_time = S.event_load_time
+AND T.filename = S.filename
+AND T.id = S.id WHEN NOT MATCHED THEN
+INSERT
+VALUES (S.event_load_time,
+        S.filename,
+        S.id,
+        S.status,
+        S.status_comment,
+        S.event_real_start,
+        S.event_real_stop,
+        S.event_planned_start,
+        S.event_planned_stop,
+        S.total_event_duration,
+        S.event_comment,
+        S.cell_id,
+        S.cell_site,
+        S.cell_node,
+        S.equipment_name,
+        S.cell_terminate,
+        S.terminate_start,
+        S.terminate_stop,
+        S.terminate_duration,
+        S.terminate_state,
+        S.i01,
+        S.i02,
+        '${PART}');
