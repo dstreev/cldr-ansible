ba68943fab122fcc037755d3271f1b0f240fd14f
David Streever <david@streever.com>
2018-06-30 14:11:06 +0200

    Test Queries

---
+++ hdp/sdlc/tests/acid_scenario_3.sql
@@ -0,0 +1,24 @@
+USE acid_${HIVE_USER};
+
+
+SELECT COUNT(1),
+       MIN(S.terminate_start),
+       MAX(S.terminate_start)
+FROM acid_3_ym_dataset S
+WHERE YEAR = 2018
+  AND MONTH = 5
+  AND terminate_start >= '2018-10-23'
+  AND terminate_start <= '2018-10-25';
+
+
+ALTER TABLE acid_3_ym_dataset PARTITION (YEAR=2018,
+                                              MONTH=6) COMPACT 'major';
+
+SELECT COUNT(1),
+       MIN(S.terminate_start),
+       MAX(S.terminate_start)
+FROM acid_3_ym_dataset S
+WHERE YEAR = 2018
+  AND MONTH = 6
+  AND terminate_start >= '2018-10-23'
+  AND terminate_start <= '2018-10-25';
