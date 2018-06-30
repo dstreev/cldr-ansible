b724ab885b43b46f200dc40949fcd0254e6301fe
David Streever <david@streever.com>
2018-06-30 14:12:57 +0200

    test queries.

--- hdp/sdlc/tests/acid_scenario_3.sql
+++ hdp/sdlc/tests/acid_scenario_3.sql
@@ -13,12 +13,20 @@ WHERE YEAR = 2018
 
 ALTER TABLE acid_3_ym_dataset PARTITION (YEAR=2018,
                                               MONTH=6) COMPACT 'major';
-
+-- wait for compaction to complete.
 SELECT COUNT(1),
        MIN(S.terminate_start),
        MAX(S.terminate_start)
 FROM acid_3_ym_dataset S
 WHERE YEAR = 2018
   AND MONTH = 6
+  AND terminate_start >= '2018-10-23'
+  AND terminate_start <= '2018-10-25';
+
+SELECT COUNT(1),
+       MIN(S.terminate_start),
+       MAX(S.terminate_start)
+FROM acid_3_ym_dataset S
+WHERE YEAR = 2018
   AND terminate_start >= '2018-10-23'
   AND terminate_start <= '2018-10-25';
